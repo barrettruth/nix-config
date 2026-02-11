459637ded9714e5ec4e359243798c30304ad87bb
Barrett Ruth <br.barrettruth@gmail.com>
2026-02-11 16:40:40 -0500

    fix: remove ~/.nix-profile symlink

--- home/modules/bootstrap.nix
+++ home/modules/bootstrap.nix
@@ -23,6 +23,12 @@ in
     done
   '';
 
+  home.activation.cleanDanglingLinks = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    for link in "$HOME/.nix-profile" "$HOME/.nix-defexpr"; do
+      [ -L "$link" ] && [ ! -e "$link" ] && run rm "$link"
+    done
+  '';
+
   home.activation.linkWallpapers = lib.hm.dag.entryAfter [ "createDirectories" ] ''
     src="${repoDir}/config/screen"
     dest="$HOME/img/screen"
