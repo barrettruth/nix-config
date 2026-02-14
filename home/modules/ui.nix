a36af0881bd17052512110eccfd63c4c02420dc1
Barrett Ruth <br.barrettruth@gmail.com>
2026-02-14 18:23:01 -0500

    fix(waybar): make it exclusive

--- home/modules/ui.nix
+++ home/modules/ui.nix
@@ -11,7 +11,7 @@ let
 
   mkWaybarTheme = palette: ''
     * { color: ${palette.fg}; }
-    window#waybar { background: ${palette.bg}; border-top: 2px solid ${palette.bgAlt}; }
+    window#waybar { background: ${palette.bg}; border-top: 4px solid ${palette.bgAlt}; }
     #workspaces button { background: transparent; }
     #workspaces button.active { box-shadow: inset 0 -2px ${palette.accent}; }
     #workspaces button:hover { background: ${palette.bgAlt}; }
